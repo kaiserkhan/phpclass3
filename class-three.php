08799050c6ddf0100805e3181d92f38023376d18
kaiser khan <kaiserbiman@gmail.com>
2018-07-26 00:19:17 +0600

    class3
    class3

---
+++ class-three.php
@@ -0,0 +1,209 @@
+<?php 
+
+
+//============== Class- 3 topic =========================
+
+/*Comparison Operator: 
+//============================
+$x <> $y mean not equal(same kaj kore !=)
+$x != $y  mean not equal
+$x !== $y mean not equal check variable er value and data type 
+$x <  $y
+$x <  $y
+$x >= $y mean greater or equal .
+$x <= $y  mean smaller or equal .*/
+
+/*$x = 3;
+$y = 4;
+if( $x == $y){
+	echo 'it\'s true';
+}
+else
+{
+
+	echo 'it\'s false';
+}*/
+//============================
+//Logical operator:
+//============================
+//  $x and $y
+//  $x or $y
+//  $x && $y
+//  $x || $y 
+
+/*$x = 3;
+$y = 4;
+
+If ( $x < 1 && $y > 5)
+{
+	echo "it's true";
+}
+else{
+echo "it’s false";
+}*/
+
+// its mean "or "  any one can true 
+/*$x = 5;
+$y = 6;
+if($x > 8 || $y < 5){
+echo 'its true';
+
+
+}
+else{
+    echo 'its false';
+
+}*/
+
+//============================
+// Pre-increment
+//==================
+/*$x = 10;
+
+echo ++$x;*/
+
+// Pre-decremental 
+
+//$x = 5;
+
+//echo --$x;
+
+//$x = 10;
+
+//echo $x++;
+ 
+/*$a = 5;  
+echo ++$a . "<br>";
+ 
+echo --$a . "<br>";
+ 
+echo $a++ . "<br>";
+ 
+echo $a-- . "<br>";*/
+//============================
+// Topics :  Array 
+//======================
+
+//$dist1= array ('dhaka','pabna','jessor');
+//echo $dist1[1];
+//======================  or===============
+//$dist1= ['dhaka','pabna','jessor'];
+//echo $dist1[0];
+
+//Custom index:  => array te custom index dora jayyyyyy
+//========================================================
+//     $dist1= [3=>'dhaka',6=>'pabna','jessor'];
+//    echo $dist1[6];
+
+// Topics: control structure
+//========================
+/*if( condition){
+
+    echo "its true";
+    
+    }else if(condition){
+    
+    
+    echo "1st its true";
+    
+    } else if(condition){
+    
+    echo " 2 nd its true";
+    
+    }else{
+    
+      echo "its not true";
+    
+    }*/
+     /*$x = 10;
+     
+    if( $x > 50){
+
+        echo "its true";
+        
+        }else if($x < 8){
+        
+        
+        echo "1st its true";
+        
+        } else if($x > 18){
+        
+        echo " 2 nd its true";
+        
+        }else{
+        
+          echo "its default show 'not true'";
+        
+        }*/
+
+
+        // topics: switch
+        //=====================
+
+      /*  $x = 10.5;
+
+switch($x){
+  
+	case 0 :
+	echo"x er man 0";
+  break;
+
+	case 10 :
+	echo"x er man 10";
+  break;
+	case 20 :
+	echo"x er man 20";
+  break;
+ 
+	case 30 :
+	echo"x er man 30";
+  break;
+
+	default:
+  	echo "x no data thik na";*/
+     //=================================== 
+     /* $sports = "football";
+
+      switch ($sports) {
+          case "football":
+              echo " প্রিয় খেলা ফুটবল!";
+              break;
+          case "cricket":
+              echo " প্রিয় খেলা ক্রিকেট!";
+              break;
+          default:
+              echo "ফুটবল এবং ক্রিকেট কোনটাই প্রিয় খেলা নয়!";
+
+           
+}*/
+    // topic:    LOOP 
+        //===================================
+       /* for (initial value; condition; increment/decrement) {
+           এই কোড সম্পাদিত হবে;
+        } */
+
+        // increment:
+            /*for ($number = 0; $number <= 5; $number++) {
+                echo "সংখ্যাটি হলোঃ $number <br>";
+            }*/
+
+        // decrement
+
+            /*for ($x = 20; $x >= 10; $x--){
+                echo "সংখ্যাটি হলোঃ $x <br>";
+            }*/
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+?>
